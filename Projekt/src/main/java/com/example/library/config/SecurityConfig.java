4a976d1c8234e602367e273d06377b12572c696c
MateuszSiudek <mateusz.siudek@student.up.krakow.pl>
2025-01-15 16:24:46 +0100

    logowanie

---
+++ Projekt/src/main/java/com/example/library/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.example.library.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests(authorizeRequests ->
+                authorizeRequests
+                    .requestMatchers("/login").permitAll() 
+                    .anyRequest().authenticated()
+            )
+            .formLogin(formLogin ->
+                formLogin
+                    .loginPage("/login")
+                    .permitAll()
+            )
+            .logout(logout -> 
+                logout.permitAll()
+            );
+        return http.build();
+    }
+}

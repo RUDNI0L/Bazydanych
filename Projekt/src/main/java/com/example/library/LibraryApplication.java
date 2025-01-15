995afbe03327b9eeff7c7cd59b6f9110d472a7f0
RUDNI0L <tomwoj01@gmail.com>
2025-01-15 15:05:51 +0100

    synchronizacja

--- Projekt/src/main/java/com/example/library/LibraryApplication.java
+++ Projekt/src/main/java/com/example/library/LibraryApplication.java
@@ -1,4 +1,4 @@
-package main.java.com.example.library;
+package com.example.library;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
